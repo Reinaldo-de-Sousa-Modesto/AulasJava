766c35e034843af804a4eea7f6306104dcb72336
Reinaldo-de-Sousa-Modesto <reinaldo.reism@gmail.com>
2022-02-28 21:29:14 -0300

    Primeiro commit Eclipse

---
+++ Primeiro-programa-java/src/cursopacote/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,10 @@
+package cursopacote.executavel;
+
+public class PrimeiraClasseJava {
+	
+	public static void main(String[] args) {
+		int x = 7, y = 4, z = x +x *y;
+		System.out.println("O resultado da conta maluca é: "+z);
+		System.out.println(z);
+	}	
+}
