f1312011b1943b7f4f812a4ca40485d7a2dfe7fc
Reinaldo-de-Sousa-Modesto <reinaldo.reism@gmail.com>
2022-02-28 21:54:52 -0300

    segundo teste

--- Primeiro-programa-java/src/cursopacote/executavel/PrimeiraClasseJava.java
+++ Primeiro-programa-java/src/cursopacote/executavel/PrimeiraClasseJava.java
@@ -5,6 +5,5 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 		int x = 7, y = 4, z = x +x *y;
 		System.out.println("O resultado da conta maluca é: "+z);
-		System.out.println(z);
 	}	
 }
